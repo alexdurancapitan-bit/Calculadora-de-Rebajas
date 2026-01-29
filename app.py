3942b614b3de7fec4d26d33cfd3ea0842d2533cd
alexdurancapitan-bit <alexdurancapitan@alumnos.tajamar.es>
2026-01-29 12:08:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+echo "# Calculadora-de-Rebajas" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alexdurancapitan-bit/Calculadora-de-Rebajas.git
+git push -u origin main
